1d37e477a8beb6ad2fb45be198aa0cd541fee396
alisoncosta <alisoncosta@d6902722-14f9-43c0-f226-cadacd6d9eb5>
2010-08-16 21:21:27 +0000

    Quase finalizado

--- ProjetoMilenio/src/br/com/bean4/addMaterial.java
+++ ProjetoMilenio/src/br/com/bean4/addMaterial.java
@@ -45,9 +45,7 @@ public class addMaterial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        tfMatTipo = new javax.swing.JTextField();
         tfMatQuantidade = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         botao_add_mat = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
@@ -65,8 +63,6 @@ public class addMaterial extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         tfMatQuantidade.setText("0");
-
-        jLabel1.setText("Tipo.:");
 
         jLabel2.setText("Quantidade");
 
@@ -92,7 +88,7 @@ public class addMaterial extends javax.swing.JFrame {
 
         jLabel7.setText("Outro Mat.:");
 
-        cbMatMaterial.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Combustível", "Lubrificante", "Gelo", "Rancho", "Pagamento de pescador", "Outro" }));
+        cbMatMaterial.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Gelo", "Isopor", "Energia", "Taxa de Aluguel" }));
         cbMatMaterial.addItemListener(new java.awt.event.ItemListener() {
             public void itemStateChanged(java.awt.event.ItemEvent evt) {
                 cbMatMaterialItemStateChanged(evt);
@@ -116,33 +112,36 @@ public class addMaterial extends javax.swing.JFrame {
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel7)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel8))
-                        .addGap(32, 32, 32)
+                            .addComponent(jLabel3))
+                        .addGap(36, 36, 36)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(tfMatFrequencia, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(tfMatOutros, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(tfMatCusto, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(tfMatQuantidade, javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(tfMatTipo, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(tfMatOutroMat, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(cbMatMaterial, javax.swing.GroupLayout.Alignment.LEADING, 0, 209, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(63, 63, 63)
-                        .addComponent(jLabel6)))
-                .addContainerGap(23, Short.MAX_VALUE))
+                        .addComponent(jLabel6))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel8))
+                        .addGap(32, 32, 32)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(tfMatQuantidade, javax.swing.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE)
+                            .addComponent(tfMatOutros, javax.swing.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE)
+                            .addComponent(tfMatFrequencia, javax.swing.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE)
+                            .addComponent(tfMatCusto, javax.swing.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE))))
+                .addGap(23, 23, 23))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(248, Short.MAX_VALUE)
+                .addContainerGap(236, Short.MAX_VALUE)
                 .addComponent(botao_add_mat, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel6)
                 .addGap(18, 18, 18)
@@ -153,10 +152,6 @@ public class addMaterial extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(tfMatOutroMat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel7))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(tfMatTipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
@@ -170,12 +165,12 @@ public class addMaterial extends javax.swing.JFrame {
                     .addComponent(jLabel8)
                     .addComponent(tfMatOutros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel5)
                     .addComponent(tfMatFrequencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(botao_add_mat)
-                .addGap(22, 22, 22))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -191,10 +186,9 @@ public class addMaterial extends javax.swing.JFrame {
             System.out.println(ex);
         }
 
-        String sqlinsert = "insert into mercado_addmaterial " + "(id_mercado,nome,tipo,quantidade," + "custo,outros,frequencia) values ('" +
+        String sqlinsert = "insert into mercado_addmaterial " + "(id_mercado,nome,quantidade,custo,outros,frequencia) values ('" +
                 idd_mercado + "','" +
-                registro_q_vai() + "','" +
-                tfMatTipo.getText() + "'," +
+                registro_q_vai() + "'," +
                 tfMatQuantidade.getText() + ",'" +
                 tfMatCusto.getText() + "','" +
                 tfMatOutros.getText() + "','" +
@@ -232,7 +226,6 @@ public class addMaterial extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botao_add_mat;
     private javax.swing.JComboBox cbMatMaterial;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -245,7 +238,6 @@ public class addMaterial extends javax.swing.JFrame {
     private javax.swing.JTextField tfMatOutroMat;
     private javax.swing.JTextField tfMatOutros;
     private javax.swing.JTextField tfMatQuantidade;
-    private javax.swing.JTextField tfMatTipo;
     // End of variables declaration//GEN-END:variables
 
     public Object registro_q_vai() {
