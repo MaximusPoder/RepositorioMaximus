8e84c4c672ce430244a28dd55998f19ebebe0722
jhonxd <jhonxd@d6902722-14f9-43c0-f226-cadacd6d9eb5>
2011-03-30 15:32:48 +0000

    forms modificados

--- Emalhe/src/br/com/formulario/principal/menu_principal.java
+++ Emalhe/src/br/com/formulario/principal/menu_principal.java
@@ -556,11 +556,11 @@ public class menu_principal extends javax.swing.JFrame {
     }//GEN-LAST:event_botao_entrevista_coloniaActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        new pescador_2().setVisible(true);
+        new pescador2().setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        new pescador_3().setVisible(true);
+        new pescador3().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
